0e6d9807ae1093b4d869ba0880d0ca51d2ceee2c
Михаил Проказин <bearpro@outlook.com>
2021-03-28 20:58:55 +0300

    ?

--- src/RpcTypeProvider.Runtime/RpcTypeProvider.Runtime.fs
+++ src/RpcTypeProvider.Runtime/RpcTypeProvider.Runtime.fs
@@ -13,20 +13,20 @@ type DataSource(filename:string) =
 
 type HttpMethod = Get | Post | Put | Delete
 
-    type ParamType =
-        | Unit
-        | Integer
-        | Float
-        | String
-        | List of ParamType
+type ParamType =
+    | Unit
+    | Integer
+    | Float
+    | String
+    | List of ParamType
 
-    type InterfaceMethod =
-      { Endpoint: string
-        MethodName: string
-        HttpMethod: HttpMethod
-        Remark: string
-        Params: ParamType list
-        Returns: ParamType }    
+type InterfaceMethod =
+  { Endpoint: string
+    MethodName: string
+    HttpMethod: HttpMethod
+    Remark: string
+    Params: ParamType list
+    Returns: ParamType }
 
 
 // Put the TypeProviderAssemblyAttribute in the runtime DLL, pointing to the design-time DLL
