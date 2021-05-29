b66d486bc34f20209f9754d7c2a7e00b638f727e
Михаил Проказин <bearpro@outlook.com>
2021-05-29 21:28:54 +0300

    Тесты для обработки вызова метода без параметров.

--- RpcServer.Tests/DispatcherTests.fs
+++ RpcServer.Tests/DispatcherTests.fs
@@ -16,9 +16,7 @@ type SampleApi() =
         member _.Sum a b = a + b
         member _.Mul a b = a * b
 
-let mutable ctx = { complexTypeMap = Map.empty }
-
-let apiSpec, _ = RpcTypeExporter.ApiSpecification.serializeApiSpec<ISampleApi>()
+let apiSpec, ctx = RpcTypeExporter.ApiSpecification.serializeApiSpec<ISampleApi>()
 let api = SampleApi() :> ISampleApi
 
 [<Fact>]
@@ -37,3 +35,31 @@ let ``Json params deserialized in correct array`` () =
                 { name = "b"; value = Value.Integer(2) }]))
     Assert.Equal(1, value.[0] :?> int)
     Assert.Equal(2, value.[1] :?> int)
+
+[<Fact>]
+let ``Method call dispatched right`` () =
+    let value = 
+        MemberCallDispatcher.parseMethodParams 
+            ctx 
+            (Value.Complex("Sum.params", [
+                { name = "a"; value = Value.Integer(3) }
+                { name = "b"; value = Value.Integer(4) }]))
+    let methodSpec = apiSpec.methods |> Seq.find (fun x -> x.name = "Sum")
+    let result = MemberCallDispatcher.dispatchCall api methodSpec value
+    Assert.Equal(result :?> int, 7)
+
+type IHasParameterlessMethod =
+    abstract NoParamsMethod: unit -> int
+
+type ClassWithParametrlessMethod() =
+    interface IHasParameterlessMethod with
+        member __.NoParamsMethod() = -1
+
+[<Fact>]
+let ``Parameterless method dispatched`` () =
+    let apiInstance = ClassWithParametrlessMethod() :> IHasParameterlessMethod
+    let apiSpec, ctx = serializeApiSpec<IHasParameterlessMethod>()
+    let methodSpec = apiSpec.methods |> Seq.find (fun x -> x.name = "NoParamsMethod")
+    let value = MemberCallDispatcher.parseMethodParams ctx Value.Unit
+    let result = MemberCallDispatcher.dispatchCall apiInstance methodSpec value
+    Assert.Equal(result :?> int, -1)
