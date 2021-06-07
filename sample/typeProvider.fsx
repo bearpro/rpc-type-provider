e161be17cfaad654c368db982383b15965dc6ee7
Михаил Проказин <bearpro@outlook.com>
2021-06-08 00:30:11 +0300

    XXX

--- sample/typeProvider.fsx
+++
@@ -1,4 +0,0 @@
-#r "..\\RpcTypeProvider\\src\\RpcTypeProvider.Runtime\\bin\\Debug\\typeproviders\\fsharp41\\netcoreapp3.1\\RpcTypeProvider.dll"
-
-[<Literal>] let url = "http://localhost:5000/SampleApi"
-type Rpc = RpcTypeProvider.RpcTypeProvider<url>
